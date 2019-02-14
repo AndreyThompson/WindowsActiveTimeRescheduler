e7cf651b26c231f1df4811b312cbbfda70b11bee
Delirus <andrey.thompson@gmail.com>
2019-02-14 10:58:47 +0300

    Initial commit

--- WindowsActiveTimeRescheduler/Program.cs
+++ WindowsActiveTimeRescheduler/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Win32;
 
 namespace WindowsActiveTimeRescheduler
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
+      var maxHours = 18;
+
+      var dt = DateTime.Now;
+
+      var ActiveHoursStart = dt.Subtract(new TimeSpan(1, 0, 0)).Hour;
+      var ActiveHoursEnd = dt.Add(new TimeSpan(maxHours - 1, 0, 0)).Hour;
+
+      Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\WindowsUpdate\\UX\\Settings","ActiveHoursStart", ActiveHoursStart);
+      Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\WindowsUpdate\\UX\\Settings","ActiveHoursEnd", ActiveHoursEnd);
     }
   }
 }
